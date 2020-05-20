e244e454351faf7a5d49673da2ca2353f6447a1c
yoyoshingu <leo@shingu.ac.kr>
2020-05-20 13:00:44 +0900

    처음 git 설치 초기화

--- 01hello/src/main/java/com/example/a01hello/MainActivity.java
+++ 01hello/src/main/java/com/example/a01hello/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 빈카추가
+        // 빈칸추가 2
     }
 }
